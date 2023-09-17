3d36458795f84be5def63a67b6f2458c3612b34e
mahatasourav <115480215+mahatasourav@users.noreply.github.com>
2023-09-17 20:17:26 +0530

    Add files via upload

---
+++ Hackathon Project/index.js
@@ -0,0 +1,16 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+
+
+
+
+    <link rel="stylesheet" href="index.js">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
